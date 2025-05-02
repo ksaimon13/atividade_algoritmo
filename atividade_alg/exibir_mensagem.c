77cddaaa1732e40e7f0357eb810d229e1db3dd41
moshage <daniel.moshage@agrotools.com.br>
2025-05-02 18:10:39 -0400

    Adicionando projeto C

---
+++ atividade_alg/exibir_mensagem.c
@@ -0,0 +1,361 @@
+#include <stdio.h>
+
+#pragma region Funcoes Atividade 1.1
+
+void exibir_mensagem() {
+	printf("Aprendendo procedimentos em C!\n\n");
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 1.2
+
+void saudacao(const char* nome) {
+	printf("Seja bem-vindo(a) %s\n\n", nome);
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 1.3
+
+void soma(float num1, float num2) {
+
+	float resultado = num1 + num2;
+
+	printf("O resultado da soma e %f", resultado);
+}
+
+void subtracao(float num1, float num2) {
+
+	float resultado = num1 - num2;
+
+	printf("\nO resultado da subtracao e %f", resultado);
+}
+
+void multiplicacao(float num1, float num2) {
+
+	float resultado = num1 * num2;
+
+	printf("\nO resultado da multiplicacao e %f", resultado);
+}
+
+void divisao(float num1, float num2) {
+
+	if (num2 != 0) {
+		float resultado = num1 / num2;
+
+		printf("\nO resultado da Divisao: %.2f\n", resultado);
+	}
+	else {
+		printf("\nErro: divisao por zero!");
+	}
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 2.1
+
+void calcular_quadrado(float numero) {
+	int resultado = numero * numero;
+	printf("\nO quadrado do numero digitado e: %d\n", resultado);
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 2.2
+
+void verificar_paridade(int num) {
+	int resultado;
+
+	resultado = num % 2;
+
+	if (resultado == 0) {
+		printf("1\n");
+	}
+	else {
+		printf("0\n");
+	};
+}
+#pragma endregion
+
+#pragma region Funcoes Atividade 2.3
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 3.1
+
+void calcular_fatorial(int num) {
+
+	int result = num;
+
+	for (int i = num; i > 0; i--)
+	{
+		result = result * i;
+
+	};
+
+	printf("\nO fatorial do numero digitado eh: %d\n", result);
+
+}
+#pragma endregion
+
+#pragma region Funcoes Atividade 3.2
+
+void verificar_primo(int num) {
+
+	int resultado = num;
+	int cont_div = 0;
+
+	for (int i = 1; i <= num; i++)
+	{
+		resultado = num % i;
+
+		if (resultado == 0) {
+			cont_div++;
+		}
+	};
+
+	if (cont_div == 2) {
+		printf("\n1\n");
+	}
+	else {
+		printf("\n0\n");
+
+	};
+
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 3.3
+
+void calcular_potencia(int base, int expoente) {
+
+	int resultado = 1;
+
+	for (int i = 1; i <= expoente; i++)
+	{
+		resultado = resultado * base;
+	};
+
+	printf("\nO resultado eh: %d\n", resultado);
+}
+
+#pragma endregion
+
+#pragma region Funcoes Atividade 4.1
+void operacaoSimples(int operacao_digitada) {
+	float num1, num2;
+	printf("\nDigite dois numeros: ");
+
+	if (scanf_s("%f %f", &num1, &num2) != 2) {
+		printf("Erro ao ler os numeros.\n\n");
+		return 1;
+	}
+
+	switch (operacao_digitada) {
+	case 1:
+		soma(num1, num2);		
+	case 2:
+		subtracao(num1, num2);		
+	case 3:
+		multiplicacao(num1, num2);		
+	case 4:
+		divisao(num1, num2);
+	case 5:
+		calcular_potencia(num1, num2);
+	default:
+		return 0;
+	}
+}
+
+void operacaoComplexa(int operacao_digitada) {
+	int num1;
+
+	printf("\nDigite um numero inteiro nao negativo: ");
+	scanf_s("%d", &num1);
+
+	switch (operacao_digitada) {
+	case 6:
+		calcular_fatorial(num1);
+	case 7:
+		verificar_primo(num1);
+	default:
+		return 0;
+	}
+}
+
+void menu() {
+	int operacao_digitada;
+
+	printf("\nDigite uma opcao;\n");
+	printf("\n Para soma, digite 1; ");
+	printf("\n Para subtracao, digite 2; ");
+	printf("\n Para multiplicacao, digite 3; ");
+	printf("\n Para divisao, digite 4; ");
+	printf("\n Para Potenciação, digite 5; ");
+	printf("\n Para Fatorial, digite 6; ");
+	printf("\n Para Verificacao de numero primo digite 7 \n\n; ");
+
+	scanf_s("%d", &operacao_digitada);
+
+	if (operacao_digitada == 0 || operacao_digitada > 7) {
+		return 0;
+	};
+
+	if (operacao_digitada < 6) {
+		operacaoSimples(operacao_digitada);
+	}
+	else {
+		operacaoComplexa(operacao_digitada);
+	}
+	menu();
+}
+#pragma endregion
+
+#pragma region Funcoes Atividade 4.2
+
+int fibonacci(int n) {
+
+	if (n <= 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else
+		return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void exibir_fibonacci(int n) {
+	printf("Sequência de Fibonacci com %d termos:\n", n);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", fibonacci(i));
+	}
+	printf("\n");
+}
+
+int getFibonacci() {
+	int n;
+	printf("Digite um número inteiro positivo: ");
+	scanf_s("%d", &n);
+	
+	exibir_fibonacci(n);
+	
+	printf("O %dº termo da sequência de Fibonacci é: %d\n", n, fibonacci(n - 1));
+
+	return 0;
+}
+#pragma endregion
+
+
+int main() {
+
+#pragma region Chamando Atividade 1.1	
+
+	exibir_mensagem();
+
+#pragma endregion
+
+#pragma region Chamando Atividade 1.2	
+
+	saudacao("Kaique");
+	saudacao("Leticia");
+	saudacao("Professor");
+
+#pragma endregion
+
+#pragma region Chamando Atividade 1.3	
+
+	float num1, num2;
+	printf("\nDigite dois numeros: ");
+
+	if (scanf_s("%f %f", &num1, &num2) != 2) {
+		printf("Erro ao ler os numeros.\n\n");
+		return 1;
+	}
+
+	soma(num1, num2);
+	subtracao(num1, num2);
+	multiplicacao(num1, num2);
+	divisao(num1, num2);
+
+#pragma endregion
+
+#pragma region Chamando Atividade 2.1
+
+	int numero;
+	printf("\nDigite um numero: ");
+	scanf_s("%d", &numero);
+	calcular_quadrado(numero);
+
+#pragma endregion
+
+#pragma region Chamando Atividade 2.2
+	int numpar;
+
+	printf("\nDigite um numero inteiro: ");
+
+	scanf_s("%d", &numpar);
+
+	verificar_paridade(numpar);
+
+#pragma endregion
+
+#pragma region Chamando Atividade 2.3
+
+#pragma endregion
+
+#pragma region Chamando Atividade 3.1
+
+	int numfatorial;
+
+	printf("\nDigite um numero inteiro nao negativo: ");
+
+	scanf_s("%d", &numfatorial);
+
+	calcular_fatorial(numfatorial);
+
+#pragma endregion
+
+#pragma region Chamando Atividade 3.2
+
+	int numprimo;
+
+	printf("\nDigite um numero inteiro: ");
+
+	scanf_s("%d", &numprimo);
+
+	verificar_primo(numprimo);
+
+#pragma endregion
+
+#pragma region Chamando Atividade 3.3
+
+	int num_base;
+	int num_exp;
+
+	printf("\nDigite um numero base: ");
+
+	scanf_s("%d", &num_base);
+
+	printf("\nDigite um numero expoente: ");
+
+	scanf_s("%d", &num_exp);
+
+	calcular_potencia(num_base, num_exp);
+
+	return 0;
+
+#pragma endregion
+
+#pragma region Chamando Atividade 4.1
+
+menu();
+
+#pragma endregion
+
+#pragma region Chamando Atividade 4.2
+
+getFibonacci();
+
+#pragma endregion
+}
---
+++ atividade_alg/saudacao.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void saudacao(char nome) {
+	printf("seja bem vindo(a) %c", nome);
+};
